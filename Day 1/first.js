9c6ff12440126866dd38068dcceedb903b98443d
unknown <sontakkejinendra17@gmail.com>
2024-02-22 17:29:03 +0530

    updated Day 1 Commit

--- Day 1/first.js
+++ Day 1/first.js
@@ -49,3 +49,38 @@ console.log(actorAge)
 const pi = 3.14
 
 console.log(pi)
+
+// Data Type
+// primitive data type : predefined data type total 7 are there.
+// non-primitve data type: userdefined data types like object etc.
+
+let age1 = 10
+console.log(typeof(age1), age1)
+let userName = "Jinendra"
+console.log(typeof(userName), userName)
+let isNo = false
+console.log(typeof(isNo), isNo)
+let actor = null
+console.log(typeof(actor), actor)
+let x1 = BigInt('123');
+console.log(typeof(x1), x1)
+let y1 = Symbol('hello')
+console.log(typeof(y1), y1)
+
+
+// objects
+
+const student = {
+    fullName : 'Rahul Kumar',
+    age : 30,
+    cgpa : 8.2,
+    isPass : true
+};
+
+// obejct is collection os properties and a property is an association between key and value.
+console.log(student)
+console.log(student['fullName'])
+console.log(student.age)
+student['age'] = student['age'] + 1
+console.log(student.age) 
+
